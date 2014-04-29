cf9e8df4de73b60c05a99b70d9078ca873edefde
MDYarma <mdyarma@gmail.com>
2014-04-30 01:45:50 +0200

    Update BB. Falta una mica...

--- src/compartit/BB.java
+++ src/compartit/BB.java
@@ -5,39 +5,40 @@ import java.util.*;
  * 
  * Thanks to Nathan Brixius for his QAP explanations.
  */
-public class BB {
+public class BB extends SolucionadorQAP{
     /**
      * @param 
      */
     private double fita;
     private final GLB glb = new GLB();
-   
-    /**
-     * Creadora
-     */
-    public BB(){}
-	
+    private QAP qap_init;
+
+    public BB(CalcularAfinitats a, CalcularDistancies d) {
+        super(a, d);
+    }
+
     /**
      * Retorna el vector d'assignacions
      */
     public int[] calcularAssignacions(double[][] afinitats, double[][] distancies){
-        int[] assign = new int[afinitats.length];
-        double cost = 0;
+        qap_init = new QAP(distancies, afinitats);
+        int[] assign = new int[afinitats.length]; //results
+        double cost = Double.POSITIVE_INFINITY; //results.objective
+        int[] v = new int[assign.length];
+        for(int x : v) x = -1;
         Stack<Node> s = new Stack();
-        s.push(new Node(new QAP(distancies, afinitats)));
+        s.push(new Node(qap_init, v));
         while(!s.empty()){
             Node n = s.pop();
             if(n.fita <= cost ){
-                if (true/*EASY BITACHES*/){
-                    //ENUMERATE BIATCHES
-                }
-                else{
-                    n.fita = glb.calcularFita(n.qap.freq, n.qap.dist);
-                    if (n.fita <= cost){
-                        Node[] vn = new Node[0];
-                        for(Node sn : vn){
-                            s.push(sn);
-                        }
+                cost = n.fita;
+                assign = n.currassign;
+                if (true/*ONLY 3 POSITIONS LEFT TO ASSIGN*/){
+                    /*BACKTRACK THEM AND PICK BEST*/
+                } else  {
+                    Node[] vn = n.Branch(); 
+                    for(Node sn : n.Branch()){
+                        s.push(sn);
                     }
                 }
             }
@@ -45,18 +46,30 @@ public class BB {
         return assign;
     }
     
-    private Branch Branching(Node n, double fita, double[][] S){
-        double[] rowSum = new double[n.size()];
-        double[] colSum = new double[n.size()];
-        for (int i = 0; i < n.size(); i++) {
-            for (int j = 0; j < n.size(); j++) {
-                rowSum[i] += S[i][j];
-                colSum[j] += S[i][j];
+
+
+    //IDEA PARA BB1 y BB2
+    //===================
+    //Convertir esta en abstract y implementar Weak Branching
+    //y Strong Branching en clases distintas.
+    //--------------------------------------------------------
+    //Ahora mismo: weak branching
+    protected Branch Branching(Node n){
+        return CoreBranching(n);
+    }
+    
+    private Branch CoreBranching(Node n){
+        double[] rowSum = new double[n.C.length];
+        double[] colSum = new double[n.C.length];
+        for (int i = 0; i < n.C.length; i++) {
+            for (int j = 0; j < n.C.length; j++) {
+                rowSum[i] += n.C[i][j];
+                colSum[j] += n.C[i][j];
             }
         }     
         int rowind = 0, colind = 0;
         double rowbest = rowSum[0], colbest = colSum[0];
-        for (int i=1; i<n.size(); ++i){
+        for (int i=1; i<n.C.length; ++i){
             if(rowbest < rowSum[i]){
                 rowbest = rowSum[i];
                 rowind = i;
@@ -78,14 +91,28 @@ public class BB {
     private class Node{
         public QAP qap;
         public double fita;
+        public double[][] C;
+        public int[] currassign;
+        
         public int size(){
             return qap.size();
         }
-        public Node(QAP q){
+        public Node(QAP q, int[] v){
             qap = q;
-            //GLB g = new GLB();
-            //fita = g.calcularFita(this.qap.freq, this.qap.dist);
+            currassign = v;
+            GLB g = new GLB();
+            fita = g.calcularFita(this.qap.freq, this.qap.dist);
+            //C = g.BLABLABLA Matriu Costes
         }
+        public Node[] Branch(){
+            Branch b = Branching(this);
+            // HAY QUE HACERLO
+            //REMEMBER TO SORT THEM 
+            //(recommend using a map to place them when finding and then sorting
+            // the keyset. Then getting stuff from map in that order.)
+            return new Node[0];
+        }
+        
     }
     
     private class QAP{
