99e910a034c4097829bb513d9860b21508083bb1
MDYarma <mdyarma@gmail.com>
2014-05-02 11:26:53 +0200

    BB Acabat.
    No s'ha debugat encara.

--- src/compartit/BB.java
+++ src/compartit/BB.java
@@ -34,7 +34,20 @@ public class BB extends SolucionadorQAP{
                 cost = n.fita;
                 assign = n.currassign;
                 if (n.isAlmostSolved()){
-                    /*BACKTRACK THEM AND PICK BEST*/
+                    int[][] whatsleft = n.whatsLeft();
+                    ArrayList<int[]> ss = permutations(whatsleft[1]);
+                    for(int[] x : ss){
+                        int[] xx = n.currassign.clone();
+                        for(int i = 0; i<3; ++i){
+                            xx[whatsleft[2][i]] = x[i];
+                        }
+                        double newcost = n.qap.costOf(xx);
+                        if( newcost < cost){
+                            assign = xx;
+                            cost = newcost;
+                        }
+                    }
+                    
                 } else  {
                     Node[] vn = n.Branch(); 
                     for(Node sn : n.Branch()){
@@ -46,8 +59,28 @@ public class BB extends SolucionadorQAP{
         return assign;
     }
     
+    private ArrayList<int[]> permutations(int[] a) {
+        ArrayList<int[]> ret = new ArrayList<int[]>();
+        permutation(a, 0, ret);
+        return ret;
+    }
 
+    private void permutation(int[] a, int pos, ArrayList<int[]> list){
+        if(a.length - pos == 1)
+            list.add(a.clone());
+        else
+            for(int i = pos; i < a.length; i++){
+                swap(a, pos, i);
+                permutation(a, pos+1, list);
+                swap(a, pos, i);
+            }
+    }
 
+    private void swap(int[] arr, int i, int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
     //IDEA PARA BB1 y BB2
     //===================
     //Convertir esta en abstract y implementar Weak Branching
@@ -100,9 +133,9 @@ public class BB extends SolucionadorQAP{
         public Node(QAP q, int[] v){
             qap = q;
             currassign = v;
-            //GLB g = new GLB();
-            fita = GLB.calcularFita(this.qap.freq, this.qap.dist);
-            //C = g.BLABLABLA Matriu Costes
+            GLB g = new GLB();
+            fita = g.calcularFita(this.qap.freq, this.qap.dist);
+            C = g.lawler;
         }
         public Node[] Branch(){
             Branch b = Branching(this);
@@ -129,6 +162,29 @@ public class BB extends SolucionadorQAP{
             int count = 0;
             for(int x : currassign) if (x==-1) ++count;
             return (count<=3);
+        }
+        public int[][] whatsLeft(){
+            int[] llocs = new int[3], objs = new int[3];
+            int[] checklist = new int[currassign.length];
+            int k = 0;
+            for(int i = 0; i<currassign.length; ++i){
+                if(currassign[i]==-1){
+                    objs[k] = i;
+                    k++;
+                }
+                else{
+                    checklist[currassign[i]] = 1;
+                }
+            }
+            k=0;
+            for(int i = 0; i<currassign.length; ++i){
+                if(checklist[i]!=1){
+                    llocs[k] = i;
+                    ++k;
+                }
+            }
+            int[][] res = {objs,llocs};
+            return res;
         }
         
         private int[] newAssign(int[] v, int i, int j){
