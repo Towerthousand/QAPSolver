9915a1cfbf1d1dbfbae9b291ddd0dc04af9dea7b
Towerthousand <dani.torramilans@gmail.com>
2014-05-03 21:42:35 +0200

    Solucio i SolucionadorQAP nomes amb 2 matrius

--- src/compartit/Solucio.java
+++ src/compartit/Solucio.java
@@ -3,12 +3,17 @@ package compartit;
 public class Solucio {
 	public int[] assignacions; //s'actualitzarà cada cop que es truqui a recalcular
     private final SolucionadorQAP solucionador;
-	public Solucio(SolucionadorQAP s, Objecte[] objs, Lloc[] llocs) { //li passes un objecte BB o un Heuristic
+	public Solucio(SolucionadorQAP s, Objecte[] objs, Lloc[] llocs) throws Exception { //li passes un objecte BB o un Heuristic
     	solucionador = s;
     	recalcular(objs, llocs);
     }
+
+	public Solucio(SolucionadorQAP s) throws Exception { //li passes un objecte BB o un Heuristic
+    	solucionador = s;
+    	assignacions = solucionador.solucionar();
+    }
 	
-	public void recalcular(Objecte[] objs, Lloc[] llocs) {
+	public void recalcular(Objecte[] objs, Lloc[] llocs) throws Exception {
 		assignacions = solucionador.solucionar(objs, llocs);
 	}
 }
